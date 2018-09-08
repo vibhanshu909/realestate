2a0e23c959a04a3303f4f322f58a5639fde41a37
vibhanshu <vibhanshu909@gmail.com>
2018-09-09 03:56:50 +0530

    using mongodb atlas for production

--- src/config/main.js
+++ src/config/main.js
@@ -2,7 +2,7 @@ import db from 'mongoose';
 
 const config = {
     secret: "+4qqu4ot3c61((zq!f@!*#60j2eb%(_(s#f+h-!xs+48o*#sgf7o>v`(KHN'>T|w83Z2v]MT_#g?<o~)LEJ,ADu]sQXps&=",
-    database: 'mongodb://localhost:27017/realestate',
+    database: "mongodb://developer_codemitter:dejVOjrJJG2JUzCb@realestatecluster-shard-00-00-c1pfm.mongodb.net:27017,realestatecluster-shard-00-01-c1pfm.mongodb.net:27017,realestatecluster-shard-00-02-c1pfm.mongodb.net:27017/realestate_production?ssl=true&replicaSet=RealEstateCluster-shard-0&authSource=admin&retryWrites=true",
     // database: "mongodb://vibhanshu:vibhanshu@testing-pr3rm.mongodb.net/real_estate"
     // database: "mongodb://vibhanshu:vibhanshu@testing-shard-00-00-pr3rm.mongodb.net:27017,testing-shard-00-01-pr3rm.mongodb.net:27017,testing-shard-00-02-pr3rm.mongodb.net:27017/test?ssl=true&replicaSet=testing-shard-0&authSource=admin"
 };
