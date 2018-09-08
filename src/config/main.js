0f6ddac21da93fa7d25e5e37497678d698dd1368
vibhanshu <vibhanshu909@gmail.com>
2018-09-09 04:07:04 +0530

    update mongdb atlas URI

--- src/config/main.js
+++ src/config/main.js
@@ -2,14 +2,14 @@ import db from 'mongoose';
 
 const config = {
     secret: "+4qqu4ot3c61((zq!f@!*#60j2eb%(_(s#f+h-!xs+48o*#sgf7o>v`(KHN'>T|w83Z2v]MT_#g?<o~)LEJ,ADu]sQXps&=",
-    database: "mongodb://developer_codemitter:dejVOjrJJG2JUzCb@realestatecluster-shard-00-00-c1pfm.mongodb.net:27017,realestatecluster-shard-00-01-c1pfm.mongodb.net:27017,realestatecluster-shard-00-02-c1pfm.mongodb.net:27017/realestate_production?ssl=true&replicaSet=RealEstateCluster-shard-0&authSource=admin&retryWrites=true",
+    database: "mongodb://developer_codemitter:dejVOjrJJG2JUzCb@realestatecluster-shard-00-00-c1pfm.mongodb.net:27017,realestatecluster-shard-00-01-c1pfm.mongodb.net:27017,realestatecluster-shard-00-02-c1pfm.mongodb.net:27017/realestate_production?ssl=true&replicaSet=RealEstateCluster-shard-0&authSource=admin",
     // database: "mongodb://vibhanshu:vibhanshu@testing-pr3rm.mongodb.net/real_estate"
     // database: "mongodb://vibhanshu:vibhanshu@testing-shard-00-00-pr3rm.mongodb.net:27017,testing-shard-00-01-pr3rm.mongodb.net:27017,testing-shard-00-02-pr3rm.mongodb.net:27017/test?ssl=true&replicaSet=testing-shard-0&authSource=admin"
 };
 
 // mongoose.Promise = global.Promise;
 // Connecting to the database
-db.connect(config.database)
+db.connect(config.database, { useNewUrlParser: true })
 .then(() => {
     console.log("Successfully connected to the database");
 }).catch(err => {
