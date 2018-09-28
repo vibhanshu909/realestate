160982c7490e2b80575433bc9d87ea04bc13ef9c
vibhanshu <vibhanshu909@gmail.com>
2018-09-28 18:03:59 +0530

    mongodb_uri updated

--- src/config/main.js
+++ src/config/main.js
@@ -2,7 +2,7 @@ import db from 'mongoose';
 
 const config = {
     secret: "+4qqu4ot3c61((zq!f@!*#60j2eb%(_(s#f+h-!xs+48o*#sgf7o>v`(KHN'>T|w83Z2v]MT_#g?<o~)LEJ,ADu]sQXps&=",
-    database: 'mongodb://localhost:27017/realestate',
+    database: 'mongodb://developer_codemitter:dejVOjrJJG2JUzCb@realestatecluster-shard-00-00-c1pfm.mongodb.net:27017,realestatecluster-shard-00-01-c1pfm.mongodb.net:27017,realestatecluster-shard-00-02-c1pfm.mongodb.net:27017/realestate_production?ssl=true&replicaSet=RealEstateCluster-shard-0&authSource=admin',
 };
 
 // mongoose.Promise = global.Promise;
